a7472f127a656930f92379f165cd6f79bd4aa27f
brndt <belacquarium@gmail.com>
2022-03-01 00:52:56 +0100

    fix: optimize imports

--- src/Readings/Domain/ReadingByPeriod.php
+++ src/Readings/Domain/ReadingByPeriod.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 
 namespace Electricity\Readings\Domain;
 
-use Closure;
-
 final class ReadingByPeriod
 {
     public function __construct(public readonly string $period, public readonly string $reading)
     {
     }
 
-    public static function comparator(): Closure
+    public static function comparator(): \Closure
     {
         return fn(ReadingByPeriod $current, ReadingByPeriod $next): int => $current->reading <=> $next->reading;
     }
